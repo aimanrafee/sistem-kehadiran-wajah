29bc40a5607ea1955cb4b9905bb76119a77b2066
Aiman Mohd <39325438+aimanrafee@users.noreply.github.com>
2026-02-04 09:11:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,9 @@
-const CACHE_NAME = 'kehadiran-v1';
-const ASSETS = [
-  '/sistem-kehadiran-wajah/',
-  '/sistem-kehadiran-wajah/index.html',
-  '/sistem-kehadiran-wajah/script.js',
-  '/sistem-kehadiran-wajah/models/tiny_face_detector_model-weights_manifest.json',
-  // Tambah semua path fail model & gambar rujukan anda di sini
-];
-
+// sw.js - Service Worker Ringkas
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  console.log('Service Worker: Installed');
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  // Biarkan request berjalan seperti biasa
+  e.respondWith(fetch(e.request));
 });
